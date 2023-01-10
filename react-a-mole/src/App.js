5aa35782d594455b072f5951105e88933fbbb87f
Gia Madonna <giamadonna1@gmail.com>
2023-01-10 12:48:18 -0800

    Could not get score function to work

--- react-a-mole/src/App.js
+++ react-a-mole/src/App.js
@@ -17,7 +17,7 @@ function App() {
     }
 
     return (
-      <div>
+      <div style={{'display': 'flex' }}>
         {hills}
       </div>
     )
--- react-a-mole/src/Components/MoleContainer.jsx
+++ react-a-mole/src/Components/MoleContainer.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import Mole from './Mole'
+import EmptySlot from './EmptySlot'
 
 function MoleContainer(props){
-    return(
+    let [displayMole, setDisplayMole] = useState(false)
+
+    const bopAMole = (e) => {
+        props.setScore(props.score + 1)
+        setDisplayMole(false)
+    }
+
+    return (
         <div>
-            <h2> Mole Container </h2>
-            <Mole />
+            {displayMole==true ? <Mole onClick ={bopAMole} setDisplayMole={setDisplayMole} />
+             : <EmptySlot setDisplayMole={setDisplayMole}></EmptySlot>}
         </div>
     )
 }
 
 export default MoleContainer
+
+// I've tried multiple different ways and no matter what I do
+// my set score won't increase. Is there something I'm missing?
